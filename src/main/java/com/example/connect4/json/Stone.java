e9ed233c74ea8e11bc9c0ce75b16c3c66efd8bc0
wallenborn <wallenborn@novo-euw3.hq.novosec.intern>
2020-01-13 14:29:21 +0100

    API

---
+++ src/main/java/com/example/connect4/json/Stone.java
@@ -0,0 +1,39 @@
+/**************************************************************************
+ * Project     : connect-4-player
+ * File        : Stone.java
+ * Language    : Java
+ * Platform    : J2EE
+ * Author      : wallenborn
+ * Created     : 13.01.2020
+ * Synopsis    : 
+ **************************************************************************/
+
+package com.example.connect4.json;
+
+
+/**
+ * Stone.
+ *
+ */
+public class Stone {
+
+  private Integer position;
+
+  
+  /**
+   * @return the position
+   */
+  public Integer getPosition() {
+    return position;
+  }
+
+  
+  /**
+   * @param position the position to set
+   */
+  public void setPosition(Integer position) {
+    this.position = position;
+  }
+  
+  
+}
